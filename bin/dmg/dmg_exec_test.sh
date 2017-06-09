8cdbbc416d2ee268f0d3c16634a19015d4b536a9
leeonky <leeonky@gmail.com>
2017-06-09 14:02:15 +0000

    docker exec -i in pipeline mode

--- bin/dmg/dmg_exec_test.sh
+++ bin/dmg/dmg_exec_test.sh
@@ -3,6 +3,7 @@
 
 setUp() {
 	mock_function sudo
+	mock_function if_pipeline "return 1"
 	IMAGE_CONTAINER=image_container
 }
 
@@ -18,6 +19,13 @@ test_exec_start_before_exec() {
 
 	mock_verify dmg_start HAS_CALLED_WITH
 	mock_verify sudo HAS_CALLED_WITH docker exec -it image_container echo 'hello world'
+}
+
+test_exec_command_in_pipeline() {
+	mock_function if_pipeline "return 0"
+	dmg_main exec cat
+	mock_verify dmg_start HAS_CALLED_WITH
+	mock_verify sudo HAS_CALLED_WITH docker exec -i image_container cat
 }
 
 
